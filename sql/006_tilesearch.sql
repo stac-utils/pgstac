b018b2943e58c1936e1d3ff8f20e00dd3ff9ce6d
David Bitner <bitner@dbspatial.com>
2021-08-09 11:02:19 -0500

    make output as featurecollection, add fields option

--- sql/006_tilesearch.sql
+++ sql/006_tilesearch.sql
@@ -1,37 +1,55 @@
 SET SEARCH_PATH to pgstac, public;
 
-CREATE OR REPLACE FUNCTION xyzsearchrecords(
-    IN _x int,
-    IN _y int,
-    IN _z int,
+DROP FUNCTION IF EXISTS geometrysearch;
+CREATE OR REPLACE FUNCTION geometrysearch(
+    IN geom geometry,
     IN queryhash text,
+    IN fields jsonb DEFAULT NULL,
     IN _scanlimit int DEFAULT 10000,
     IN _limit int DEFAULT 100,
     IN _timelimit interval DEFAULT '5 seconds'::interval,
     IN skipcovered boolean DEFAULT TRUE
-) RETURNS setof items AS $$
+) RETURNS jsonb AS $$
 DECLARE
     search searches%ROWTYPE;
     curs refcursor;
     _where text;
     query text;
     iter_record items%ROWTYPE;
+    out_records jsonb[] := '{}'::jsonb[];
     exit_flag boolean := FALSE;
     counter int := 1;
     scancounter int := 1;
     remaining_limit int := _scanlimit;
-    tile geometry;
     tilearea float;
     unionedgeom geometry;
     clippedgeom geometry;
     unionedgeom_area float := 0;
     prev_area float := 0;
+    excludes text[];
+    includes text[];
 
 BEGIN
     SELECT * INTO search FROM searches WHERE hash=queryhash;
-    tile := st_transform(tileenvelope(_z, _x, _y), 4326);
-    tilearea := st_area(tile);
-    _where := format('%s AND st_intersects(geometry, %L::geometry)', search._where, tile);
+    tilearea := st_area(geom);
+    _where := format('%s AND st_intersects(geometry, %L::geometry)', search._where, geom);
+
+    IF fields IS NOT NULL THEN
+        IF fields ? 'fields' THEN
+            fields := fields->'fields';
+        END IF;
+        IF fields ? 'exclude' THEN
+            excludes=textarr(fields->'exclude');
+        END IF;
+        IF fields ? 'include' THEN
+            includes=textarr(fields->'include');
+            IF array_length(includes, 1)>0 AND NOT 'id' = ANY (includes) THEN
+                includes = includes || '{id}';
+            END IF;
+        END IF;
+    END IF;
+    RAISE NOTICE 'fields: %s, includes: %s, excludes: %s', fields, includes, excludes;
+
     FOR query IN SELECT * FROM partition_queries(_where, search.orderby) LOOP
         query := format('%s LIMIT %L', query, remaining_limit);
         RAISE NOTICE '%', query;
@@ -40,7 +58,7 @@ BEGIN
             FETCH curs INTO iter_record;
             EXIT WHEN NOT FOUND;
 
-            clippedgeom := st_intersection(tile, iter_record.geometry);
+            clippedgeom := st_intersection(geom, iter_record.geometry);
 
             IF unionedgeom IS NULL THEN
                 unionedgeom := clippedgeom;
@@ -58,8 +76,11 @@ BEGIN
             prev_area := unionedgeom_area;
 
             RAISE NOTICE '% % % %', st_area(unionedgeom)/tilearea, counter, scancounter, ftime();
-            RETURN NEXT iter_record;
-
+            IF fields IS NOT NULL THEN
+                out_records := out_records || filter_jsonb(iter_record.content, includes, excludes);
+            ELSE
+                out_records := out_records || iter_record.content;
+            END IF;
 
             IF counter > _limit
                 OR scancounter > _scanlimit
@@ -76,20 +97,54 @@ BEGIN
         EXIT WHEN exit_flag;
         remaining_limit := _scanlimit - scancounter;
     END LOOP;
-RETURN;
+
+    RETURN jsonb_build_object(
+        'type', 'FeatureCollection',
+        'features', array_to_json(out_records)::jsonb
+    );
 END;
 $$ LANGUAGE PLPGSQL;
 
-
-CREATE OR REPLACE FUNCTION xyzsearch(
-    IN _x int,
-    IN _y int,
-    IN _z int,
+DROP FUNCTION IF EXISTS geojsonsearch;
+CREATE OR REPLACE FUNCTION geojsonsearch(
+    IN geojson jsonb,
     IN queryhash text,
+    IN fields jsonb DEFAULT NULL,
     IN _scanlimit int DEFAULT 10000,
     IN _limit int DEFAULT 100,
     IN _timelimit interval DEFAULT '5 seconds'::interval,
     IN skipcovered boolean DEFAULT TRUE
 ) RETURNS jsonb AS $$
-SELECT jsonb_agg(to_jsonb(r)) FROM xyzsearchrecords (_x, _y, _z, queryhash, _scanlimit, _limit, _timelimit, skipcovered) r;
+    SELECT * FROM geometrysearch(
+        st_geomfromgeojson(geojson),
+        queryhash,
+        fields,
+        _scanlimit,
+        _limit,
+        _timelimit,
+        skipcovered
+    );
+$$ LANGUAGE SQL;
+
+DROP FUNCTION IF EXISTS xyzsearch;
+CREATE OR REPLACE FUNCTION xyzsearch(
+    IN _x int,
+    IN _y int,
+    IN _z int,
+    IN queryhash text,
+    IN fields jsonb DEFAULT NULL,
+    IN _scanlimit int DEFAULT 10000,
+    IN _limit int DEFAULT 100,
+    IN _timelimit interval DEFAULT '5 seconds'::interval,
+    IN skipcovered boolean DEFAULT TRUE
+) RETURNS jsonb AS $$
+    SELECT * FROM geometrysearch(
+        st_transform(tileenvelope(_z, _x, _y), 4326),
+        queryhash,
+        fields,
+        _scanlimit,
+        _limit,
+        _timelimit,
+        skipcovered
+    );
 $$ LANGUAGE SQL;
