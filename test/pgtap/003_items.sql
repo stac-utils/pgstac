5c9b204c1c3569be3da368d4079fb7fe60102268
David Bitner <bitner@dbspatial.com>
2021-08-23 15:25:27 -0500

    add functions for bulk create_items and upsert_items

--- test/pgtap/003_items.sql
+++ test/pgtap/003_items.sql
@@ -1,11 +1,14 @@
 SELECT has_table('pgstac'::name, 'items'::name);
 
--- SELECT is_indexed('pgstac'::name, 'items'::name, ARRAY['datetime','id']);
--- SELECT is_indexed('pgstac'::name, 'items'::name, 'properties');
+
 SELECT is_indexed('pgstac'::name, 'items'::name, 'geometry');
 SELECT is_indexed('pgstac'::name, 'items'::name, 'collection_id');
+SELECT is_indexed('pgstac'::name, 'items'::name, 'datetime');
+SELECT is_indexed('pgstac'::name, 'items'::name, 'end_datetime');
+SELECT is_indexed('pgstac'::name, 'items'::name, 'properties');
+SELECT is_indexed('pgstac'::name, 'items'::name, 'collection_id');
+SELECT is_indexed('pgstac'::name, 'items'::name, ARRAY['datetime,id']);
 
--- SELECT has_type('pgstac'::name, 'item'::name);
 SELECT is_partitioned('pgstac'::name,'items'::name);
 
 
@@ -14,12 +17,13 @@ SELECT has_function('pgstac'::name, 'delete_item', ARRAY['text']);
 SELECT has_function('pgstac'::name, 'create_item', ARRAY['jsonb']);
 SELECT has_function('pgstac'::name, 'update_item', ARRAY['jsonb']);
 SELECT has_function('pgstac'::name, 'upsert_item', ARRAY['jsonb']);
+SELECT has_function('pgstac'::name, 'create_items', ARRAY['jsonb']);
+SELECT has_function('pgstac'::name, 'upsert_items', ARRAY['jsonb']);
 
 
 
 SELECT has_function('pgstac'::name, 'analyze_empty_partitions', NULL);
--- SELECT has_function('pgstac'::name, 'backfill_partitions', NULL);
--- SELECT has_function('pgstac'::name, 'items_trigger_stmt_func', NULL);
+SELECT has_function('pgstac'::name, 'items_update_triggerfunc', NULL);
 
 
 SELECT has_view('pgstac'::name, 'all_items_partitions'::name, 'all_items_partitions view exists');
