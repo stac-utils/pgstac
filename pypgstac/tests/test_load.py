eca2e3ab2269da1b8c6a51a506335e5bc625e93c
Nathan Zimmerman <npzimmerman@gmail.com>
2022-02-22 09:05:07 -0600

    Add test load of non-ndjson collection

--- pypgstac/tests/test_load.py
+++ pypgstac/tests/test_load.py
@@ -3,11 +3,13 @@ import asyncio
 from pathlib import Path
 import unittest
 
-from pypgstac.pypgstac import load_ndjson, loadopt, tables
+from pypgstac.pypgstac import loadopt, tables
+from pypgstac.load import load_json, load_ndjson
 
 HERE = Path(__file__).parent
 TEST_DATA_DIR = HERE.parent.parent / "test" / "testdata"
-TEST_COLLECTIONS = TEST_DATA_DIR / "collections.ndjson"
+TEST_COLLECTIONS_NDJSON = TEST_DATA_DIR / "collections.ndjson"
+TEST_COLLECTION_JSON = TEST_DATA_DIR / "collection.json"
 TEST_ITEMS = TEST_DATA_DIR / "items.ndjson"
 
 
@@ -18,7 +20,14 @@ class LoadTest(unittest.TestCase):
         """Test pypgstac data loader."""
         asyncio.run(
             load_ndjson(
-                str(TEST_COLLECTIONS),
+                str(TEST_COLLECTIONS_NDJSON),
+                table=tables.collections,
+                method=loadopt.upsert,
+            )
+        )
+        asyncio.run(
+            load_json(
+                str(TEST_COLLECTION_JSON),
                 table=tables.collections,
                 method=loadopt.upsert,
             )
